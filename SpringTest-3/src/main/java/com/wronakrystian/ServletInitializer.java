aeb34bc3c2febefd9b797dfc9d3449728d8e7946
krystek77 <krystian.wrona77@gmail.com>
2019-10-16 16:44:33 +0200

    Initial code

---
+++ SpringTest-3/src/main/java/com/wronakrystian/ServletInitializer.java
@@ -0,0 +1,13 @@
+package com.wronakrystian;
+
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
+
+public class ServletInitializer extends SpringBootServletInitializer {
+
+	@Override
+	protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+		return application.sources(SpringTest3Application.class);
+	}
+
+}
