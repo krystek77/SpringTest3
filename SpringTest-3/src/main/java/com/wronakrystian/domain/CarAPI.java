1cab1fe13d3380c523dba8567273a4b463a3652d
krystek77 <krystian.wrona77@gmail.com>
2019-10-18 09:42:42 +0200

    Remove import

--- SpringTest-3/src/main/java/com/wronakrystian/domain/CarAPI.java
+++ SpringTest-3/src/main/java/com/wronakrystian/domain/CarAPI.java
@@ -3,7 +3,6 @@ package com.wronakrystian.domain;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
